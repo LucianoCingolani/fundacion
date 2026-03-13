fba71c0b3a34112a93ca50f0fc9fec914a699c7e
Luciano Cingolani <lulucingolani@gmail.com>
2026-03-13 16:20:15 -0300

    cambios varioss

--- fundacion_app/urls.py
+++ fundacion_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('donaciones/nueva/', views.registrar_donacion, name='registrar_donacion'),
+    path('comunicacion/enviar/', views.enviar_mail_masivo, name='enviar_mail_masivo'),
 ]

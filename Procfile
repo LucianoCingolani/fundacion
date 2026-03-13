df69a7c167e2a79762c2fc3b8d24357e36bd8e33
Luciano Cingolani <lulucingolani@gmail.com>
2026-03-13 12:31:50 -0300

    WHITENOISE

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn fundacion.wsgi
+web: python manage.py collectstatic && gunicorn fundacion.wsgi
--- fundacion/settings.py
+++ fundacion/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'fundacion_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,5 +120,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_FILES_DIRS = [
+    BASE_DIR / 'fundacion_app/static', 
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGES = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = ['localhost', 'web-production-58ef3.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://web-production-58ef3.up.railway.app']
